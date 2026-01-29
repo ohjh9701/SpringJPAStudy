2e4152d360f2e1ef71adb1b79f591f6213391efe
ohjh9701 <ohjh4875@gmail.com>
2026-01-29 12:25:07 +0900

    [생성] JPA 형식에 맞게 domain Customer 생성

---
+++ SpringJPAStudy/src/main/java/com/study/domain/Customer.java
@@ -0,0 +1,43 @@
+package com.study.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@SequenceGenerator(
+		name = "CUSTOMER_SEQ_GEN",
+		sequenceName = "CUSTOMER_SEQ",
+		initialValue = 1,
+		allocationSize = 1
+		)
+@Table(name = "CUSTOMER")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUSTOMER_SEQ_GEN")
+	@Column(name = "NO")
+	long no;
+	
+	@Column(name = "ID")
+	String id;
+	
+	@Column(name = "PASSWORD")
+	String password;
+	
+	@Column(name = "NAME")
+	String name;
+	
+	@Column(name = "PHONE")
+	String phone;
+	
+	@Column(name = "EMAIL")
+	String email;
+
+	
+}
